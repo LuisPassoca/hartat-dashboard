43334f45f9922123ce0b726a37f19fbec1a6e6fe
LuisPassoca <luiseduardopmagalhaes@gmail.com>
2026-04-16 22:27:07 -0300

    Began working on an Image Manager

--- src/components/ImageManager.jsx
+++ src/components/ImageManager.jsx
@@ -1,13 +1,34 @@
+import { useEffect, useRef, useState } from 'react'
+import './ImageManager.css'
+
 function ImageManager() {
+    const [search, setSearch] = useState('')
+
+    const fetchImages = () => {
+        return fetch('/api/images', {
+            method: 'GET'
+        })
+    }
+
+    useEffect(() => {
+        const timeout = setTimeout(async () => {
+            
+        })
+
+
+    }, [search])
 
     return(
         <>
-            <div className="search-bar">
-                <input type="text" placeholder="Buscar imagem..." />
+            <div className="searchbar">
+                <input type="text" placeholder="Buscar imagem..." onChange={({ target }) => setSearch(target.value)} />
                 <i class="fa-solid fa-magnifying-glass"></i>
             </div>
 
-            
+            <div className='images-container'>
+                {images.map(image => <img key={image.id} src={image.url} loading="lazy" />)}
+            </div>
+
         </>
     )
 }
